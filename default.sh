26c596d2ef1d828a92baaad58104dcacd6e21bae
ymederuiter <155627120+ymederuiter@users.noreply.github.com>
2025-09-17 02:44:07 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install openssh-server curl htop unattended-upgrades cron -y
+sudo apt install openssh-server curl htop unattended-upgrades cron nano -y
 
 sudo timedatectl set-timezone Europe/Amsterdam
 sudo dpkg-reconfigure -f noninteractive unattended-upgrades
