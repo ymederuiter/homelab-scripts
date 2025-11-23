adffb05ed0891d4053ebfcf2089219d586e493eb
ymederuiter <155627120+ymederuiter@users.noreply.github.com>
2025-11-23 22:40:01 +0100

    Changing usermod command + adding reboot

--- docker.sh
+++ docker.sh
@@ -20,4 +20,5 @@ echo \
 sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker yme
+sudo usermod -aG docker $USER
+sudo reboot
