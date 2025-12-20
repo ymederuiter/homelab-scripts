f5d617425a1fc44345addd0411cef3877c7202bc
Yme de Ruiter <155627120+ymederuiter@users.noreply.github.com>
2025-12-20 16:53:21 +0100

    Added iputils-ping package to installation

--- default.sh
+++ default.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install openssh-server curl htop unattended-upgrades cron nano -y
+sudo apt install openssh-server curl htop unattended-upgrades cron nano iputils-ping -y
 
 sudo timedatectl set-timezone Europe/Amsterdam
 sudo dpkg-reconfigure -f noninteractive unattended-upgrades
